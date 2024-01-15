0d34332bb9c5d691498edd2ce5926d38208b5c1e
eshita241 <eshitachokhani2003@gmail.com>
2024-01-16 00:42:27 +0530

    feat:saved changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ import { FaCode } from "react-icons/fa6";
 const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div><div className='h-52 md:h-72 w-auto rounded-t-xl relative group' style ={{background:`url(${imgUrl})`, backgroundSize:"cover"}}>
-      <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
+      <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0  group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
         <a href=" https://github.com" className='h-14 w-14 border-2 relative rounded-full border-white group/link group-hover/link:border-green'>
         <FaCode className='h-10 w-10 m-2 absolute cursor-pointer text-green hover:text-green'/>
         </a>
